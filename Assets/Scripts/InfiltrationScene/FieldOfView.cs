cbfaadfc31e680922a843fa45b053d0e62bfe215
Geundol222 <geundol222@gmail.com>
2023-06-22 19:12:08 +0900

    feat : Player Interactor 구현
    플레이어가 벽에 가까워졌을 때 플레이어 앞 방향의 벽을 체크한 뒤 높은벽과 낮은 벽에 따라 숨는 애니메이션을 출력하고 NavMeshAgent를 통해 벽쪽으로 붙는 기능 구현

--- Assets/Scripts/InfiltrationScene/FieldOfView.cs
+++ Assets/Scripts/InfiltrationScene/FieldOfView.cs
@@ -33,6 +33,8 @@ public class FieldOfView : MonoBehaviour
 {
     Mesh viewMesh;
 
+    public PlayerInteractor player;
+
     [SerializeField] int edgeResolveIterations;
     [SerializeField] float edgeDstThreshold;
     [SerializeField] MeshFilter viewMeshFilter;
@@ -100,7 +102,7 @@ public class FieldOfView : MonoBehaviour
 
     private void AddList(GameObject obj)
     {
-        if (findList.Count <= 0)
+        if (findList.Count <= 0 && !player.IsHide)
             findList.Add (obj);
         else
         {
@@ -111,7 +113,7 @@ public class FieldOfView : MonoBehaviour
 
     private void CheckFindList()
     {
-        if (findList.Count > 0)
+        if (findList.Count > 0 && !player.IsHide)
             isFind = true;
         else
             isFind = false;
