3f0c41f66d3fff619c3e06217acfceae6d9fef2c
Geundol222 <geundol222@gmail.com>
2023-06-22 20:30:54 +0900

    feat : Player DIe 구현
    플레이어가 죽으면 게임오버 UI가 나오도록 구현

--- Assets/Scripts/InfiltrationScene/PlayerController.cs
+++ Assets/Scripts/InfiltrationScene/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator anim;
+    private NavMeshAgent agent;
+    private Collider col;
+    private PlayerInput input;
     private int hp;
 
     private void Awake()
     {
+        input = GetComponent<PlayerInput>();
+        col = GetComponent<Collider>();
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
         hp = 100;
     }
 
     public void TakeDamage(int damage)
     {
+        anim.SetTrigger("Hit");
+
         hp -= damage;
 
         if (hp <= 0)
@@ -23,6 +35,21 @@ public class PlayerController : MonoBehaviour
 
     private void Die()
     {
-        
+        col.enabled = false;
+        input.enabled = false;
+        agent.enabled = false;
+        StartCoroutine(DieRoutine());
+    }
+
+    IEnumerator DieRoutine()
+    {
+        anim.SetInteger("Hp", -1);
+
+        Time.timeScale = 0.3f;
+        yield return new WaitForSecondsRealtime(1.5f);
+        Time.timeScale = 1f;
+        yield return new WaitForSeconds(4f);
+        GameManager.UI.ShowPopUpUI<GameOverUI>("UI/PopUpUI/GameOverUI");
+        yield break;
     }
 }
---
+++ Assets/Scripts/UI/PopUpUI/GameOverUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverUI : PopUpUI
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // TODO : 게임 재시작 혹은 메인메뉴 버튼 구현
+    }
+}
